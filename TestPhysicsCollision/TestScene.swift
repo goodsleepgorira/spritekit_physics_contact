eadb283b4931838bfde891da29d3b29fd70b3cb2
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-08-20 20:33:36 +1200

    Test

--- TestPhysicsCollision/TestScene.swift
+++ TestPhysicsCollision/TestScene.swift
@@ -7,24 +7,26 @@ import SpriteKit
 
 class TestScene:SKScene {
     
+    var birdBrown:SKSpriteNode!
+    var testLabel:SKLabelNode!
+    
     //現在シーン設定時の呼び出しメソッド
     override func didMoveToView(view: SKView) {
-        
-        //観測しやすくするため重力を少し弱くする。
-        self.physicsWorld.gravity = CGVector(dx:0, dy:-5.0)
-        
-        //画面端に物理ボディを設定する。
-        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
         
         //背景画像のノードを作成する。
         let backNode = SKSpriteNode(imageNamed: "night")
         backNode.size = self.frame.size
         backNode.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
         
+        //ラベルノードを作成する。
+        testLabel = SKLabelNode(text: "テスト開始")
+        testLabel.position = CGPointMake(CGRectGetMidX(self.frame), self.frame.height-50)
+        
         //鳥のノードを作成する。
-        let birdBrown = SKSpriteNode(imageNamed: "bird_brown")
+        birdBrown = SKSpriteNode(imageNamed: "bird_brown")
         let birdRed = SKSpriteNode(imageNamed: "bird_red")
         let birdBlue = SKSpriteNode(imageNamed: "bird_blue")
+        
         
         //鳥の位置を設定する。
         birdBrown.position = CGPointMake(CGRectGetMidX(self.frame), self.frame.height-100)
@@ -37,22 +39,18 @@ class TestScene:SKScene {
         birdBlue.physicsBody = SKPhysicsBody(texture: birdBlue.texture!, size: birdBlue.size)
         
         //カテゴリのビットマスクを設定する。
-        birdBrown.physicsBody?.categoryBitMask = 1
-        birdRed.physicsBody?.categoryBitMask = 1
-        birdBlue.physicsBody?.categoryBitMask = 1
+        birdBrown.physicsBody?.categoryBitMask = 0b0001
+        birdRed.physicsBody?.categoryBitMask = 0b0010
+        birdBlue.physicsBody?.categoryBitMask = 0b0100
 
-        //衝突するカテゴリのビットマスクを設定する。
-        birdBrown.physicsBody?.collisionBitMask = 1
-        birdRed.physicsBody?.collisionBitMask = 1
-        birdBlue.physicsBody?.collisionBitMask = 1
-
-        //他ノードに衝突されても動かなくする。
-        birdBrown.physicsBody!.dynamic = false
-        birdRed.physicsBody!.dynamic = false
-        birdBlue.physicsBody!.dynamic = false
+        //重力の影響を受けないようにする。
+        birdBrown.physicsBody!.affectedByGravity = false
+        birdRed.physicsBody!.affectedByGravity = false
+        birdBlue.physicsBody!.affectedByGravity = false
         
         //ノードをシーンに追加する。
         self.addChild(backNode)
+        self.addChild(testLabel)
         self.addChild(birdBrown)
         self.addChild(birdRed)
         self.addChild(birdBlue)
@@ -61,17 +59,14 @@ class TestScene:SKScene {
     
     
     //画面タッチ時の呼び出しメソッド
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
-        //タッチした座標のノードを取得する。
+        //画面をタッチした座標に移動するアクションを作成する。
         let location = touches.first!.locationInNode(self)
-        let node = nodeAtPoint(location) as! SKSpriteNode
-        
-        if(node.physicsBody != nil) {        
-            //タッチしたノードを落とす。
-            node.physicsBody!.dynamic = true
-        }
+        let action = SKAction.moveTo(CGPoint(x:location.x, y:location.y), duration:0.1)
 
+        //アクションを実行する。
+        birdBrown.runAction(action)
     }
-    
+
 }
